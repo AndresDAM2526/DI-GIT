84d2344e4eb2fe8a1169ab07abc1f125bcbbba91
AndresDAM2526 <williaman.corgar@educa.jcyl.es>
2025-10-14 13:27:13 +0200

    Ejercicio 5 iniciado

---
+++ Ej5/ej_5/bin/ej_5.dart
@@ -0,0 +1,8 @@
+/*
+  Utiliza la libreía http para realizar una petición GET a una API pública(como JSONPlaceHolder).
+  Usa async y await para manejar la respuestas asíncrona y muestra los datos en consola
+*/
+
+void main() {
+  
+}
---
+++ Ej5/ej_5/test/ej_5_test.dart
@@ -0,0 +1,8 @@
+import 'package:ej_5/ej_5.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
