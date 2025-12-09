f8579ebf74eee74524149652e90fbc81ba15e678
AndresDAM2526 <williaman.corgar@educa.jcyl.es>
2025-12-09 13:24:51 +0100

    Se implementa la base de datos

--- Tema 4/ejercicios_gastos_ingresos/lib/main.dart
+++ Tema 4/ejercicios_gastos_ingresos/lib/main.dart
@@ -17,7 +17,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: PantallaPrincipal());
+    return Consumer<DatabaseProvider>(
+      builder: (_, value, __) => MaterialApp(home: PantallaPrincipal()),
+    );
   }
 }
 
@@ -36,7 +38,6 @@ class PantallaPrincipal extends StatelessWidget {
             ListTile(
               title: Text("Insertar datos"),
               onTap: () {
-                Navigator.pop(context);
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => Formulario()),
@@ -46,7 +47,6 @@ class PantallaPrincipal extends StatelessWidget {
             ListTile(
               title: Text("Transacciones"),
               onTap: () {
-                Navigator.pop(context);
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => Transacciones()),
@@ -74,10 +74,14 @@ class _FormularioState extends State<Formulario> {
   List<String> tiposIngresos = ["Nómina"];
   List<String> tiposGastos = ["Alquiler", "Internet", "Comida", "Cine"];
   List<Widget> elementosToggle = [Text("Gasto"), Text("Ingreso")];
-  List<bool> seleccionado = [false, false];
+  List<bool> tipoSeleccionado = [false, false];
   String? categoriaSeleccionada;
   final validadFormulario = GlobalKey<FormState>();
   TextEditingController? dinero = TextEditingController();
+  // se crea esta variable para poder acceder a ese widget y reiniciarlo,
+  //esto lo hacemos porque si seleccionabamos un elemento del dropdown y cambiabamos a otro tipo daba error
+  //de esta manera, primero reiniciamos el dropdown y luego se muestra
+  GlobalKey<FormFieldState> keyDropDown = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
@@ -93,39 +97,62 @@ class _FormularioState extends State<Formulario> {
                 child: Card(
                   child: Column(
                     children: [
-                      Text("Tipo"),
-                      ToggleButtons(
-                        fillColor: seleccionado[0] ? Colors.red : Colors.green,
-                        isSelected: seleccionado,
-                        onPressed: (index) {
-                          setState(() {
-                            for (int i = 0; i < seleccionado.length; i++) {
-                              seleccionado[i] = (i == index);
-                            }
-                          });
-                        },
-                        children: elementosToggle,
+                      Container(
+                        margin: EdgeInsets.all(20),
+                        child: Column(
+                          children: [
+                            Text("Tipo"),
+                            ToggleButtons(
+                              fillColor: tipoSeleccionado[0]
+                                  ? Colors.red
+                                  : Colors.green,
+                              isSelected: tipoSeleccionado,
+                              onPressed: (index) {
+                                setState(() {
+                                  for (
+                                    int i = 0;
+                                    i < tipoSeleccionado.length;
+                                    i++
+                                  ) {
+                                    tipoSeleccionado[i] = (i == index);
+                                    keyDropDown.currentState!.reset();
+                                  }
+                                });
+                              },
+                              children: elementosToggle,
+                            ),
+                          ],
+                        ),
                       ),
+
                       Container(
                         margin: EdgeInsets.all(20),
                         child: Column(
                           children: [
                             Text("Categoría"),
                             DropdownButtonFormField(
-                              hint: seleccionado[0]
+                              validator: (value) {
+                                if (categoriaSeleccionada!.isEmpty) {
+                                  "Seleccione una categoría";
+                                }
+                              },
+                              key: keyDropDown,
+                              hint: tipoSeleccionado[0]
                                   ? Text("Seleccione un gasto")
-                                  : Text("Seleccione un ingreso"),
-                              items: seleccionado[0]
+                                  : tipoSeleccionado[1]
+                                  ? Text("Seleccione un ingreso")
+                                  : Text("Seleccione un tipo de movimiento"),
+                              items: tipoSeleccionado[0]
                                   ? tiposGastos.map((String gasto) {
                                       return DropdownMenuItem<String>(
                                         value: gasto,
                                         child: Text(gasto),
                                       );
                                     }).toList()
-                                  : tiposIngresos.map((String gasto) {
+                                  : tiposIngresos.map((String ingreso) {
                                       return DropdownMenuItem<String>(
-                                        value: gasto,
-                                        child: Text(gasto),
+                                        value: ingreso,
+                                        child: Text(ingreso),
                                       );
                                     }).toList(),
                               onChanged: (value) {
@@ -140,8 +167,9 @@ class _FormularioState extends State<Formulario> {
                       Container(
                         margin: EdgeInsets.all(20),
                         child: TextFormField(
+                          controller: dinero,
                           validator: (value) {
-                            if(value!.isEmpty){
+                            if (value!.isEmpty) {
                               return "Introduzca una cantidad";
                             }
                           },
@@ -151,15 +179,29 @@ class _FormularioState extends State<Formulario> {
                       Container(
                         margin: EdgeInsets.all(20),
                         child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: tipoSeleccionado[0]
+                                ? Colors.red
+                                : Colors.green,
+                          ),
                           onPressed: () {
+                            setState(() {
+                              if (validadFormulario.currentState!.validate()) {
+                                context
+                                    .read<DatabaseProvider>()
+                                    .anadirMovimiento(
+                                      tipoSeleccionado[0] ? "Gasto" : "Ingreso",
+                                      categoriaSeleccionada!,
+                                      double.parse(dinero!.text),
+                                    );
+                                dinero!.clear();
+                                categoriaSeleccionada = null;
+                              }
+                            });
                           },
                           child: Text(
                             "Guardar",
-                            style: TextStyle(
-                              color: seleccionado[0]
-                                  ? Colors.red
-                                  : Colors.green,
-                            ),
+                            style: TextStyle(color: Colors.black),
                           ),
                         ),
                       ),
@@ -180,6 +222,48 @@ class Transacciones extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Center(child: Text("Transacciones"))),
+      body: context.watch<DatabaseProvider>().movimientos.isEmpty
+          ? Center(child: Text("No hay datos aún"))
+          : Center(
+              child: ListView.builder(
+                itemCount: context.read<DatabaseProvider>().movimientos.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: EdgeInsets.all(10),
+                    child: Card(
+                      color:
+                          context
+                                  .read<DatabaseProvider>()
+                                  .movimientos[index]['tipo'] ==
+                              "Gasto"
+                          ? Colors.red
+                          : Colors.green,
+                      child: ListTile(
+                        leading:
+                            context
+                                    .read<DatabaseProvider>()
+                                    .movimientos[index]['tipo'] ==
+                                "Gasto"
+                            ? Icon(Icons.arrow_downward)
+                            : Icon(Icons.arrow_upward),
+                        title: Text(
+                          context
+                              .read<DatabaseProvider>()
+                              .movimientos[index]['categoria']
+                              .toString(),
+                        ),
+                        subtitle: Text(
+                          context
+                              .read<DatabaseProvider>()
+                              .movimientos[index]['dinero']
+                              .toString(),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
     );
   }
 }
@@ -201,10 +285,10 @@ class DatabaseProvider extends ChangeNotifier {
     final dbPath = join(await databaseFactory.getDatabasesPath(), 'cuenta.db');
     final database = await databaseFactory.openDatabase(dbPath);
     await database.execute('''CREATE TABLE IF NOT EXISTS cuenta(
-      id integer PRIMARY KEY AUTOINCREMENT,
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
       tipo TEXT NOT NULL,
       categoria TEXT NOT NULL,
-      dinero double NOT NULL
+      dinero REAL NOT NULL
     )''');
     return database;
   }
@@ -215,20 +299,18 @@ class DatabaseProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> anadirGasto(String categoria, double dinero) async {
+  Future<void> anadirMovimiento(
+    String tipo,
+    String categoria,
+    double dinero,
+  ) async {
     final db = await database;
     await db.insert('cuenta', {
       'tipo': 'Gasto',
       'categoria': categoria,
       'dinero': dinero,
     });
-  }
-  Future<void> anadirIngreso(String categoria, double dinero) async {
-    final db = await database;
-    await db.insert('cuenta', {
-      'tipo': 'Ingreso',
-      'categoria': categoria,
-      'dinero': dinero,
-    });
+    cargarDatos();
+    notifyListeners();
   }
 }
